442c029c03ff6c44c2f50806e1e6c5733571ba13
Paul Sobolik <psobolik@zoho.com>
2025-12-28 08:53:01 -0500

    Improved code

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,13 @@
 import './styles/App.sass'
-import * as React from "react";
 import Canvas from "./Canvas.tsx";
 
 function App() {
-    const [width] = React.useState(1000);
-    const [height] = React.useState(800);
-
     return (<>
-        <header>Canvas Fun!</header>
-        <Canvas width={width} height={height}></Canvas>
-        <footer id="footer"><a href={"https://github.com/psobolik/canvas-fun.git"}>source</a>
+        <header>More Canvas Fun!</header>
+        <Canvas/>
+        <footer id="footer"><a href={"https://github.com/psobolik/more-canvas-fun.git"}>source</a>
         </footer>
         <script type="module" src="/src/main.ts"></script>
     </>)
 }
-
 export default App
--- src/Canvas.tsx
+++ src/Canvas.tsx
@@ -1,29 +1,19 @@
-import * as React from "react";
+import React from "react";
 import Point from "./Point";
 import {CanvasTheme} from "./canvas-theme.ts";
 import Util from "./Util.ts";
 
-interface CanvasProps {
-    width: number;
-    height: number;
-}
-
-const Canvas: React.FunctionComponent<CanvasProps> = (props) => {
+const Canvas: React.FunctionComponent = () => {
+    const width = 1000;
+    const height = 800;
     const BORDER_SIZE = 1;
 
-    const [point, setPoint] = React.useState<Point>(new Point(0, 0));
-    const [cellWidth] = React.useState(() => {
-        return props.width / 50;
-    })
-    const [rowCount, setRowCount] = React.useState(0);
-    const [colCount, setColCount] = React.useState(0);
+    const cellWidthRef = React.useRef(width / 50);
+    const rowCountRef = React.useRef(height / cellWidthRef.current);
+    const colCountRef = React.useRef(width / cellWidthRef.current);
 
-    React.useEffect(() => {
-        setColCount(props.width / cellWidth);
-    }, [props.width, cellWidth]);
-    React.useEffect(() => {
-        setRowCount(props.height / cellWidth);
-    }, [props.height, cellWidth]);
+    const [point, setPoint] = React.useState<Point>(new Point(0, 0));
+
     React.useEffect(() => {
         const draw = () => {
             const drawBackground = (context: CanvasRenderingContext2D) => {
@@ -33,24 +23,24 @@ const Canvas: React.FunctionComponent<CanvasProps> = (props) => {
             }
             const drawGrid = (context: CanvasRenderingContext2D) => {
                 const drawCell = (context: CanvasRenderingContext2D, x: number, y: number, size: number)=> {
-                    const sz = Math.max(Math.min(cellWidth - 1, size), 2);
-                    const wh = Math.ceil(cellWidth - sz);
-                    const margin = Math.ceil((cellWidth - wh) / 2);
+                    const sz = Math.max(Math.min(cellWidthRef.current - 1, size), 2);
+                    const wh = Math.ceil(cellWidthRef.current - sz);
+                    const margin = Math.ceil((cellWidthRef.current - wh) / 2);
                     context.rect(x + margin, y + margin, wh, wh);
                 }
-                if (cellWidth === 0 || rowCount === 0 || colCount === 0) return;
+                if (cellWidthRef.current === 0 || rowCountRef.current === 0 || colCountRef.current === 0) return;
 
                 context.beginPath();
                 context.lineWidth = BORDER_SIZE;
                 context.strokeStyle = CanvasTheme.line;
                 context.fillStyle = CanvasTheme.fill;
-                for (let r = 0; r < rowCount; ++r) {
-                    for (let c = 0; c < colCount; ++c) {
-                        const x = c * cellWidth;
-                        const y = r * cellWidth;
+                for (let r = 0; r < rowCountRef.current; ++r) {
+                    for (let c = 0; c < colCountRef.current; ++c) {
+                        const x = c * cellWidthRef.current;
+                        const y = r * cellWidthRef.current;
                         const d = Util.calcDistance(x, y, point.x, point.y);
-                        const w = d / Math.min(props.width, props.height);
-                        const s = (cellWidth * w * 8) / 7;
+                        const w = d / Math.min(width, height);
+                        const s = (cellWidthRef.current * w * 8) / 7;
                         drawCell(context, x, y, s);
                     }
                 }
@@ -61,22 +51,20 @@ const Canvas: React.FunctionComponent<CanvasProps> = (props) => {
             const context: CanvasRenderingContext2D | null = canvas.getContext('2d');
             if (context == null) return;
 
-            context.save();
             drawBackground(context);
             drawGrid(context);
-            context.restore();
         }
         draw();
-    }, [point, props.width, props.height, cellWidth, colCount, rowCount]);
+    }, [point]);
 
     const mouseMove = (event: React.MouseEvent<HTMLCanvasElement>) => {
-        const halfCellWidth = cellWidth / 2;
+        const halfCellWidth = cellWidthRef.current / 2;
         const canvas = document.getElementById("canvas") as HTMLCanvasElement;
         const p = new Point(event.clientX - canvas.offsetLeft - halfCellWidth, event.clientY - canvas.offsetTop - halfCellWidth);
         if (p.x != point.x && p.x != point.y)
             setPoint(p);
     }
-    return <canvas id="canvas" width={props.width} height={props.height} onMouseMove={mouseMove}></canvas>
+    return <canvas id="canvas" width={width} height={height} onMouseMove={mouseMove}></canvas>
 }
 
 export default Canvas;
