ab410812cc8c74ab90aff9c302c97af96524a6e5
Paul Sobolik <psobolik@zoho.com>
2025-12-28 08:55:13 -0500

    Improved code

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import Canvas from "./Canvas.tsx";
 
 function App() {
     return (<>
-        <header>More Canvas Fun!</header>
+        <header>Canvas Fun!</header>
         <Canvas/>
         <footer id="footer"><a href={"https://github.com/psobolik/more-canvas-fun.git"}>source</a>
         </footer>
