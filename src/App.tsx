42c31ae90a8a53b96e6b9198f05a7a231ae44b41
Paul Sobolik <psobolik@zoho.com>
2026-03-24 09:39:41 -0400

    Size canvas dynamically

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,35 @@
 import './styles/App.sass'
 import Canvas from "./Canvas.tsx";
+import React from "react";
 
 function App() {
-    return (<>
-        <header>Canvas Fun!</header>
-        <Canvas/>
-        <footer id="footer"><a href={"https://github.com/psobolik/canvas-fun.git"}>source</a>
-        </footer>
-        <script type="module" src="/src/main.ts"></script>
-    </>)
+    const [windowSize, setWindowSize] = React.useState({
+      width: window.innerWidth,
+      height: window.innerHeight,
+    });
+
+    React.useEffect(() => {
+      const handleResize = () => {
+        const MARGIN = 50;
+        const footerHeight = document.getElementsByTagName("footer")[0].clientHeight;
+        const headerHeight = document.getElementsByTagName("header")[0].clientHeight;
+
+        setWindowSize({
+          width: window.innerWidth - MARGIN,
+          height: window.innerHeight - MARGIN - footerHeight - headerHeight,
+        });
+      };
+
+      handleResize()
+      window.addEventListener('resize', handleResize);
+      return () => { window.removeEventListener('resize', handleResize); };
+    }, []);
+  return (<>
+    <header>Canvas Fun!</header>
+    <Canvas windowSize={windowSize}/>
+    <footer id="footer"><a href={"https://github.com/psobolik/canvas-fun.git"}>source</a>
+    </footer>
+  </>)
 }
+
 export default App
--- src/Canvas.tsx
+++ src/Canvas.tsx
@@ -3,68 +3,90 @@ import Point from "./Point";
 import {CanvasTheme} from "./canvas-theme.ts";
 import Util from "./Util.ts";
 
-const Canvas: React.FunctionComponent = () => {
-    const width = 1000;
-    const height = 800;
-    const BORDER_SIZE = 1;
+interface CanvasProps {
+  windowSize: { width: number, height: number }
+}
 
-    const cellWidthRef = React.useRef(width / 50);
-    const rowCountRef = React.useRef(height / cellWidthRef.current);
-    const colCountRef = React.useRef(width / cellWidthRef.current);
+const Canvas: React.FunctionComponent<CanvasProps> = (props: CanvasProps) => {
+  const BORDER_SIZE = 1;
 
-    const [point, setPoint] = React.useState<Point>(new Point(0, 0));
+  const [point, setPoint] = React.useState<Point>(new Point(0, 0));
+  const [canvas, setCanvas] = React.useState<HTMLCanvasElement | null>(null);
 
-    React.useEffect(() => {
-        const draw = () => {
-            const drawBackground = (context: CanvasRenderingContext2D) => {
-                context.fillStyle = CanvasTheme.background;
-                context.beginPath();
-                context.fillRect(0.0, 0.0, context.canvas.width, context.canvas.height);
-            }
-            const drawGrid = (context: CanvasRenderingContext2D) => {
-                const drawCell = (context: CanvasRenderingContext2D, x: number, y: number, size: number)=> {
-                    const sz = Math.max(Math.min(cellWidthRef.current - 1, size), 2);
-                    const wh = Math.ceil(cellWidthRef.current - sz);
-                    const margin = Math.ceil((cellWidthRef.current - wh) / 2);
-                    context.rect(x + margin, y + margin, wh, wh);
-                }
-                if (cellWidthRef.current === 0 || rowCountRef.current === 0 || colCountRef.current === 0) return;
+  const context = canvas?.getContext('2d');
+  const cellWidth = Math.min(props.windowSize.width, props.windowSize.height) / 25;
 
-                context.beginPath();
-                context.lineWidth = BORDER_SIZE;
-                context.strokeStyle = CanvasTheme.line;
-                context.fillStyle = CanvasTheme.fill;
-                for (let r = 0; r < rowCountRef.current; ++r) {
-                    for (let c = 0; c < colCountRef.current; ++c) {
-                        const x = c * cellWidthRef.current;
-                        const y = r * cellWidthRef.current;
-                        const d = Util.calcDistance(x, y, point.x, point.y);
-                        const w = d / Math.min(width, height);
-                        const s = (cellWidthRef.current * w * 8) / 7;
-                        drawCell(context, x, y, s);
-                    }
-                }
-                context.stroke();
-                context.fill();
-            }
-            const canvas = document.getElementById("canvas") as HTMLCanvasElement;
-            const context: CanvasRenderingContext2D | null = canvas.getContext('2d');
-            if (context == null) return;
+  React.useEffect(() => {
+    setCanvas(document.getElementById("canvas") as HTMLCanvasElement);
+  }, [])
 
-            drawBackground(context);
-            drawGrid(context);
+  React.useEffect(() => {
+    const draw = () => {
+      const drawBackground = (context: CanvasRenderingContext2D | null) => {
+        if (!context) return;
+
+        context.fillStyle = CanvasTheme.background;
+        context.fillRect(0.0, 0.0, context.canvas.width, context.canvas.height);
+      }
+      const drawGrid = (context: CanvasRenderingContext2D | null) => {
+        if (!context) return;
+
+        const rowCount = props.windowSize.height / cellWidth;
+        const colCount = props.windowSize.width / cellWidth;
+
+        const drawCell = (context: CanvasRenderingContext2D, x: number, y: number, size: number) => {
+          const sz = Math.max(Math.min(cellWidth - 1, size), 2);
+          const wh = Math.ceil(cellWidth - sz);
+          const margin = Math.ceil((cellWidth - wh) / 2);
+          context.rect(x + margin, y + margin, wh, wh);
         }
-        draw();
-    }, [point]);
+        if (cellWidth === 0 || rowCount === 0 || colCount === 0) return;
 
-    const mouseMove = (event: React.MouseEvent<HTMLCanvasElement>) => {
-        const halfCellWidth = cellWidthRef.current / 2;
-        const canvas = document.getElementById("canvas") as HTMLCanvasElement;
-        const p = new Point(event.clientX - canvas.offsetLeft - halfCellWidth, event.clientY - canvas.offsetTop - halfCellWidth);
-        if (p.x != point.x && p.x != point.y)
-            setPoint(p);
+        context.beginPath();
+        context.lineWidth = BORDER_SIZE;
+        context.strokeStyle = CanvasTheme.line;
+        context.fillStyle = CanvasTheme.fill;
+        for (let r = 0; r < rowCount; ++r) {
+          for (let c = 0; c < colCount; ++c) {
+            const x = c * cellWidth;
+            const y = r * cellWidth;
+            const d = Util.calcDistance(x, y, point.x, point.y);
+            const w = d / Math.min(props.windowSize.width, props.windowSize.height);
+            const s = (cellWidth * w * 8) / 7;
+            drawCell(context, x, y, s);
+          }
+        }
+        context.stroke();
+        context.fill();
+      }
+      const ctx = context;
+      if (!ctx) return;
+      drawBackground(ctx);
+      drawGrid(ctx);
     }
-    return <canvas id="canvas" width={width} height={height} onMouseMove={mouseMove}></canvas>
+    draw();
+  }, [context, point, props.windowSize, cellWidth]);
+
+  const changePoint = (x: number, y: number) => {
+    if (!canvas) return;
+
+    const halfCellWidth = cellWidth / 2;
+    const p = new Point(x - canvas.offsetLeft - halfCellWidth, y - canvas.offsetTop - halfCellWidth);
+    if (p.x != point.x && p.x != point.y)
+      setPoint(p);
+  }
+  const touchMove = (event: React.TouchEvent<HTMLCanvasElement>) => {
+    event.preventDefault();
+    changePoint(event.touches[0].clientX, event.touches[0].clientY);
+  }
+
+  const mouseMove = (event: React.MouseEvent<HTMLCanvasElement>) => {
+    event.preventDefault();
+    changePoint(event.clientX, event.clientY);
+  }
+
+  return <canvas id="canvas" width={props.windowSize.width} height={props.windowSize.height} onMouseMove={mouseMove}
+                 onTouchMove={touchMove}></canvas>
 }
 
 export default Canvas;
